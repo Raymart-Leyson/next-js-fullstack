9a7fb96232c281565012a9e08ea671857978d58f
Raymart-Leyson <raymart.leyson.rl@gmail.com>
2024-05-29 21:48:08 +0800

    Add About Page Design

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,33 @@ import styles from './about.module.css'
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>We Create digital ideas that are bigger, bolder, braver and better</h1>
+        <p className={styles.description}>
+          We create digital ideas that are bigger, bolder, braver and better.
+          We believe in good ideas and flexibility and precission We're world's
+          Our Special Team best consulting & finane solution provider. wide range
+          of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h3>100+</h3>
+            <p>Completed Projects</p>
+          </div>
+          <div className={styles.box}>
+            <h3>100+</h3>
+            <p>Happy Clients</p>
+          </div>
+          <div className={styles.box}>
+            <h3>100+</h3>
+            <p>Cup Of Coffee</p>
+          </div>
+        </div>
+      </div>
       <div className={styles.imgContainer}>
-        <Image src="/about.png" alt="" fill/>
+        <Image src="/about.png" alt="About Img" className={styles.img} fill/>
       </div>
       
     </div>
--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>Logo</div>
-      <div className={styles.text}>Logo creative thoughts Agency All rights reserved</div>
+      <div className={styles.text}>Logo creative thoughts Agency © All rights reserved</div>
     </div>
     
   )
