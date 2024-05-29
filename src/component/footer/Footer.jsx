0972da46fa703621cc0144ecf861ad2dfee8c56c
Raymart-Leyson <raymart.leyson.rl@gmail.com>
2024-05-29 17:07:59 +0800

    Add HomePage Designs

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,6 +1,12 @@
+import styles from './footer.module.css'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>Logo</div>
+      <div className={styles.text}>Logo creative thoughts Agency All rights reserved</div>
+    </div>
+    
   )
 }
 
